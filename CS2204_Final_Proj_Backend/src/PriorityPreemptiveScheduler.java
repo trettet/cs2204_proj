ea49e1cfe75a67c96eb7765a7e91bede1259433e
trettet <vince0villamora@gmail.com>
2017-03-03 23:45:00 +0800

    Added tabbed interface, improved preloader interface

--- CS2204_Final_Proj_Backend/src/PriorityPreemptiveScheduler.java
+++ CS2204_Final_Proj_Backend/src/PriorityPreemptiveScheduler.java
@@ -94,14 +94,6 @@ public class PriorityPreemptiveScheduler
 
     private void echoResults()
     {
-        // DELAY simulation
-        try {
-            Thread.currentThread().sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        //
-
         int i;
         TableObject newElem;
         LinkedList<TableObject> taList = new LinkedList<>();
